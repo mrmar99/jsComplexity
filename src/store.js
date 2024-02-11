3830fb3d190398354d2de79f54e9af79a3c55f6f
mrmar99 <mrmar99@icloud.com>
2024-02-11 05:46:11 +0300

    Autodetect & set language

--- src/store.js
+++ src/store.js
@@ -1,13 +1,19 @@
 import { createStore, createEvent } from "effector";
 import contentData from "./content/data.json";
 
+console.log(navigator.language)
+
 export const changeItems = createEvent();
 export const changeLanguage = createEvent();
 export const changeSearchInput = createEvent();
 
+const languages = new Set(["ru", "en"]);
+const userLanguage = navigator.language.split('-')[0];
+const language = languages.has(userLanguage) ? userLanguage : "en";
+
 export default createStore({
   items: contentData,
-  language: "ru",
+  language,
   searchInputValue: ""
 })
   .on(changeLanguage, (store, payload) => ({
